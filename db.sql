5eafaaaa6008c2a5fb9f5d5e39791b36cf058103
ZHIXUAN YU <yu343@purdue.edu>
2016-02-12 16:32:58 -0500

    add group_id and user’s self intro

--- db.sql
+++ db.sql
@@ -3,6 +3,7 @@ create table User_info(
   uname varchar2(20) NOT NULL,
   upwd  varchar2(30) NOT NULL,
   umail varchar2(40) NOT NULL,
+  umemo varchar2(140) NOT NULL;
   primary key(user_id));
 
 create table Contact(
@@ -14,12 +15,13 @@ create table Contact(
 create table Group_info(
   user_id integer NOT NULL,
   gname varchar2(30),
+  gid integer check (gid > 0),
   primary key(user_id),
   foreign key (user_id) references User_info (user_id));
 
 INSERT INTO User_info VALUES (1,'XXX','password','xxx@gmail.com');
 INSERT INTO User_info VALUES (2, 'YYY','pass','yyy@gmail.com');
 
-INSERT INTO Contact VALUES (1, 2);
+INSERT INTO Contact VALUES (1, 2, 1);
 
 INSERT INTO Group_info VALUES (1, 'CS 448');
